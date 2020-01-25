16f2d947ee0921a762e23a79c099c0a881a35abb
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-25 22:10:16 +0300

    Реализация поиска в Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import './Search.css';
 
-const Search = () => {
-  return <h1>Search page</h1>;
-};
+class Search extends Component {
+  state = {
+    query: ''
+  };
+
+  render() {
+    const { query } = this.state;
+
+    return (
+      <div className="search">
+        <div className="search-panel-wrapper">
+          <input className="search-field"
+                 onChange={this.handleChange}
+                 value={query}
+                 placeholder="Поиск фильма"/>
+          <button className="search-btn"
+                  onClick={this.handleClick}>
+            Найти
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  handleChange = (event) => this.setState({ query: event.target.value });
+
+  handleClick = () => {};
+
+}
 
 export default Search;
