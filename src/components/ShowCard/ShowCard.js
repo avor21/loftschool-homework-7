732cdc913ce87bdd10b4824706ccab65c4a2f08f
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-26 17:38:03 +0300

    Переход на страницу сериала

--- src/components/ShowCard/ShowCard.js
+++ src/components/ShowCard/ShowCard.js
@@ -1,45 +1,37 @@
-import React, {Component} from "react";
-import {Button, Card, Icon, Image, Label} from 'semantic-ui-react'
-import {connect} from "react-redux";
-import {fetchShowRequest} from '../../actions'
+import React from "react";
+import {Card, Icon, Image, Label} from 'semantic-ui-react'
+import {Link} from "react-router-dom";
 
-class ShowCard extends Component{
-  render() {
-    const { id, name, summary,
-    image, rating: { average : avgRating} } = this.props.show;
+const ShowCard = (props) => {
+  const { id, name, summary,
+  image, rating: { average : avgRating} } = props.show;
 
-    return (
-      <Card>
-        <Card.Content>
-          <Image src={image && image.medium}
-                 size="small"
-                 floated="right"
-                 rounded >
-            {image === null ? <Label content="Image not found" icon="warning"/> : null}
-          </Image>
-          <Card.Header>{name}</Card.Header>
-          {
-            avgRating !== null
-            ? <Card.Meta>
-                <Icon color="yellow" name="star" />
-                <span>{avgRating}</span>
-              </Card.Meta>
-            : null
-          }
-          <Card.Description dangerouslySetInnerHTML={{__html: summary}} />
-        </Card.Content>
-        <Card.Content extra>
-          <Button basic fluid onClick={() => this.onClickHandler(id)}>
-            <Icon name="film"/>
-            Перейти к сериалу
-          </Button>
-        </Card.Content>
-      </Card>
-    );
-  }
+  return (
+    <Card>
+      <Card.Content>
+        <Image src={image && image.medium}
+               size="small"
+               floated="right"
+               rounded >
+          {image === null ? <Label content="Image not found" icon="warning"/> : null}
+        </Image>
+        <Card.Header>{name}</Card.Header>
+        {
+          avgRating !== null
+          ? <Card.Meta>
+              <Icon color="yellow" name="star" />
+              <span>{avgRating}</span>
+            </Card.Meta>
+          : null
+        }
+        <Card.Description dangerouslySetInnerHTML={{__html: summary}} />
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="film"/>
+        <Link to={`/shows/${id}`}>Перейти на страницу сериала</Link>
+      </Card.Content>
+    </Card>
+  );
+};
 
-  onClickHandler = (id) => this.props.fetchShowRequest(id);
-
-}
-
-export default connect(null, { fetchShowRequest })(ShowCard);
+export default ShowCard;
--- src/middlewares/showMiddleware.js
+++ src/middlewares/showMiddleware.js
@@ -1,3 +1,12 @@
-export default state => next => action => {
+import { fetchShowRequest, fetchShowSuccess, fetchShowFailure } from '../actions';
+import { show } from '../api';
+
+export default ({ dispatch }) => next => action => {
+  if (action.type === fetchShowRequest.toString()) {
+    show(action.payload)
+      .then(show => dispatch(fetchShowSuccess(show)))
+      .catch(error => dispatch(fetchShowFailure(error)))
+  }
+
   return next(action);
 };
