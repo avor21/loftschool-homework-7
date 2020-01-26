953e800c6086828e63cf6723c4a8e62e88ef5ebc
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-26 16:41:55 +0300

    ShowCard теперь функциональный компонент

--- src/components/ShowCard/ShowCard.js
+++ src/components/ShowCard/ShowCard.js
@@ -1,40 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import {Card, Icon, Image, Label} from 'semantic-ui-react'
 
-class ShowCard extends Component {
-  render() {
-    const { url, name, summary,
-      image, rating: { average : avgRating} } = this.props.show;
+const ShowCard = (props) => {
+  const { url, name, summary,
+    image, rating: { average : avgRating} } = props.show;
 
-    return (
-      <Card>
-        <Card.Content>
-          <Image src={image && image.medium}
-                 size="small"
-                 floated="right"
-                 rounded >
-            {image === null ? <Label content="Image not found" icon="warning"/> : null}
-          </Image>
-          <Card.Header>{name}</Card.Header>
-          {
-            avgRating !== null
-            ? <Card.Meta>
-                <Icon color="yellow" name="star" />
-                <span>{avgRating}</span>
-              </Card.Meta>
-            : null
-          }
-          <Card.Description dangerouslySetInnerHTML={{__html: summary}} />
-        </Card.Content>
-        <Card.Content extra>
-          <a href={url}>
-            <Icon name="film"/>
-            Перейти на страницу сериала
-          </a>
-        </Card.Content>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card>
+      <Card.Content>
+        <Image src={image && image.medium}
+               size="small"
+               floated="right"
+               rounded >
+          {image === null ? <Label content="Image not found" icon="warning"/> : null}
+        </Image>
+        <Card.Header>{name}</Card.Header>
+        {
+          avgRating !== null
+          ? <Card.Meta>
+              <Icon color="yellow" name="star" />
+              <span>{avgRating}</span>
+            </Card.Meta>
+          : null
+        }
+        <Card.Description dangerouslySetInnerHTML={{__html: summary}} />
+      </Card.Content>
+      <Card.Content extra>
+        <a href={url}>
+          <Icon name="film"/>
+          Перейти на страницу сериала
+        </a>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default ShowCard;
